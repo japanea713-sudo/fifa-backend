9ffcd5f8cad5a026d467f92fcecd547d327251ef
unknown <japanea713@gmail.com>
2026-09-11 21:50:32 +0100

    add Cors2

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
- app.add_middleware( CORSMiddleware, allow_origins=["http://localhost:5173", "https://frontend-lac-seven-59.vercel.app/"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"], )
+ app.add_middleware( CORSMiddleware, allow_origins=["http://localhost:5173", "https://frontend-lac-seven-59.vercel.app"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"], )
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
