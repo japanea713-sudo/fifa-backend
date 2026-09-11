66e5f26bffe5c8f98339061aa33e11d34edbf63f
unknown <japanea713@gmail.com>
2026-09-11 22:13:33 +0100

    hhhhh

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://frontend-lac-seven-59.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://fifa-frontend-five.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
